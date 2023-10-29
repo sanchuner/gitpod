ad7392de39e5e7a12332d400fe040dd377bfe321
sanchuner <111401609+sanchuner@users.noreply.github.com>
2023-10-29 11:27:22 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'dd3cd54d-9678-4b25-80ba-f31db821f2b9';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
